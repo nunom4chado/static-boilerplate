d1698cef342928389593b7321a41b056bad432cf
Nuno Machado <nunom4chado@gmail.com>
2019-10-05 23:58:39 +0100

    initial setup

---
+++ dist/js/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = "main";
+
+var multiply = function multiply(x) {
+  return x * 2;
+};
+
+console.log(multiply(10));
+//# sourceMappingURL=bundle.js.map
---
+++ src/js/entry.js
@@ -0,0 +1,4 @@
+const a = "main";
+
+const multiply = x => x * 2;
+console.log(multiply(10));
